9803fc4a1a4ebb5100c1a2ab9aa71371932b5433
Chris Bartlett <bartlettc@gmail.com>
2018-01-13 17:29:22 -0700

    cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
   next();
  });
 
