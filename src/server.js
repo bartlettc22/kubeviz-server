cfa71312040ec094e4fa5f38cb4a23416b8a9d1d
Chris Bartlett <bartlettc@gmail.com>
2018-01-13 20:12:52 -0700

    cors/limits

--- src/server.js
+++ src/server.js
@@ -7,10 +7,10 @@ var memcached = new Memcached('localhost:11211');
 var clusters = []
 
 
-app.use(bodyParser.json({limit: 50000000, type:'application/json'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
+app.use(bodyParser.json({limit: 50000000, type:'application/json'}).urlencoded({limit: '50mb', extended: false }));
+// app.use(bodyParser);
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
