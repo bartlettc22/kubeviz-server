1a9cba7de011a3d77c85e15768ba87be500b0cd8
Chris Bartlett <bartlettc@gmail.com>
2018-02-02 14:37:28 -0700

    more dialogflow work

--- src/controllers/dialogFlowController.js
+++ src/controllers/dialogFlowController.js
@@ -15,12 +15,24 @@ exports.post = function(req, res) {
       input_listClusters(req, res);
       break;
 
-    case "input.allClusterResources":
+    case "input.clusterResources":
       input_clusterResources(req, res);
       break;
 
     case "input.clusterRegion":
       input_clusterRegion(req, res);
+      break;
+
+    case "input.regionClusters":
+      input_regionClusters(req, res);
+      break;
+
+    case "input.lowestVersion":
+      input_lowestVersion(req, res);
+      break;
+
+    case "input.highestVersion":
+      input_highestVersion(req, res);
       break;
 
     case "input.audio.websocketCountByCluster":
@@ -48,7 +60,7 @@ function input_listClusters(req, res) {
 
     cluster_list = result.map(o => o.cluster.substr(0, o.cluster.indexOf('.')))
 
-    sendResponse(res, "I'm currently tracking the following clusters: "+cluster_list.join(', '))
+    sendResponse(res, "I'm currently tracking the following clusters: "+join_items(cluster_list))
   });
 
 }
@@ -134,7 +146,7 @@ function input_clusterResources(req, res) {
         resources = resources.filter(o => o.metadata[resource_type+".cluster"].startsWith(cluster))
       }
 
-      sendResponse(res, "There are currently "+resources.length+" "+input_resource+" running "+cluster_speech)
+      sendResponse(res, "There are currently "+resources.length.toLocaleString('en-US')+" "+input_resource+" running "+cluster_speech)
     })
   }
 }
@@ -152,9 +164,80 @@ function input_clusterRegion(req, res) {
                     .filter(o => o.metadata["kubernetes.node.cluster"].startsWith(input_cluster))
                     .map(o => o.metadata["kubernetes.node.region"])
 
-    sendResponse(res, "The "+input_cluster+" cluster is in "+dm.region_map[resources[0]]+" a.k.a. "+resources[0])
+    sendResponse(res, "The "+input_cluster+" cluster is in "+dm.region_map_voice[resources[0]]+" a.k.a. "+resources[0])
   })
 
+}
+
+function input_regionClusters(req, res) {
+
+  var input_region = req.body.result.parameters.region
+
+  mongo.MongoDbO.collection("rawData").find({}).toArray(function(err, result) {
+    dm = new dataModel(result);
+    dm.parse();
+
+    count = result.length
+    clusters = dm.data
+            .filter(o => (o.type === "kubernetes.node" &&
+                    (o.metadata["kubernetes.node.region"].includes(input_region)
+                    || dm.region_map[o.metadata["kubernetes.node.region"]].includes(input_region)
+                    )))
+            .map(o => o.metadata["kubernetes.node.cluster"].substr(0, o.metadata["kubernetes.node.cluster"].indexOf('.')))
+            .filter(onlyUnique)
+    console.log(clusters)
+    sendResponse(res, "I'm currently tracking "+join_items(clusters)+" in the "+input_region+" region")
+  })
+}
+
+function input_lowestVersion(req, res) {
+
+  mongo.MongoDbO.collection("rawData").find({}).toArray(function(err, result) {
+    dm = new dataModel(result);
+    dm.parse();
+
+    // count = result.length
+    unique_versions = dm.data
+            .filter(o => (o.type === "kops.cluster"))
+            .map(o => o.data["kops.cluster.version"])
+            .filter(onlyUnique)
+
+    unique_versions.sort(versionCompare)
+    lowest_version = unique_versions[0]
+    var clusters = dm.data
+            .filter(o => o.type === "kops.cluster")
+            .filter(o => o.data["kops.cluster.version"] === lowest_version)
+            .map(o => o.metadata["kops.cluster.cluster"].substr(0, o.metadata["kops.cluster.cluster"].indexOf('.')))
+
+
+    console.log(unique_versions)
+    sendResponse(res, "The lowest version of Kubernetes is "+lowest_version+" running on "+join_items(clusters))
+  })
+}
+
+function input_highestVersion(req, res) {
+
+  mongo.MongoDbO.collection("rawData").find({}).toArray(function(err, result) {
+    dm = new dataModel(result);
+    dm.parse();
+
+    // count = result.length
+    unique_versions = dm.data
+            .filter(o => (o.type === "kops.cluster"))
+            .map(o => o.data["kops.cluster.version"])
+            .filter(onlyUnique)
+
+    unique_versions.sort(versionCompare).reverse()
+    highest_version = unique_versions[0]
+    var clusters = dm.data
+            .filter(o => o.type === "kops.cluster")
+            .filter(o => o.data["kops.cluster.version"] === highest_version)
+            .map(o => o.metadata["kops.cluster.cluster"].substr(0, o.metadata["kops.cluster.cluster"].indexOf('.')))
+
+
+    console.log(unique_versions)
+    sendResponse(res, "The highest version of Kubernetes is "+highest_version+" running on "+join_items(clusters))
+  })
 }
 
 function input_audioWebsocketCountByCluster(req, res) {
@@ -185,7 +268,7 @@ function getPrometheusMetric(cluster_name, prometheus_url, prometheus_stat, call
   endEpoch = Math.floor(Date.now() / 1000);
   startEpoch = endEpoch - 300 // Get 5 minutes?
 
-  prometheusUrl = prometheus_url.replace(/\~/g, "")
+  prometheusUrl = prometheus_url.replace(/\~cluster\~/g, cluster_name)
   query = "query_range?query=sum("+prometheus_stat+")&start="+startEpoch+"&end="+endEpoch+"&step=60"
 
   request(prometheusUrl+query, { timeout: 5000}, (error, response, body) => {
@@ -204,6 +287,57 @@ function getPrometheusMetric(cluster_name, prometheus_url, prometheus_stat, call
   });
 }
 
+function join_items(a) {
+  return [a.slice(0, -1).join(', '), a.slice(-1)[0]].join(a.length < 2 ? '' : ' and ');
+}
+
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
+}
+
+function versionCompare(v1, v2, options) {
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
+
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+    }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        }
+        else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
 }
--- src/models/data.js
+++ src/models/data.js
@@ -30,6 +30,7 @@ var Data = function (raw_data) {
               "type": "kops.cluster",
               "metadata": {
                 "kops.cluster.uid": e.metadata.name,
+                "kops.cluster.cluster": cluster,
                 "kops.cluster.created": fmtDate(e.metadata.creationTimestamp)
               },
               "data": {
@@ -629,6 +630,26 @@ Data.prototype.region_map  = {
     "eu-west-3": "EU (Paris)",
     "sa-east-1": "South America (Sao Paulo)"
   }
+
+  Data.prototype.region_map_voice  = {
+      "us-east-2": "US East (Ohio)",
+      "us-east-1":"US East (Northern Virginia)",
+      "us-west-1":"US West (Northern California)",
+      "us-west-2":"US West (Oregon)",
+      "ap-northeast-1":"Asia Pacific (Tokyo)",
+      "ap-northeast-2":"Asia Pacific (Seoul)",
+      "ap-south-1":"Asia Pacific (Mumbai)",
+      "ap-southeast-1":"Asia Pacific (Singapore)",
+      "ap-southeast-2": "Asia Pacific (Sydney)",
+      "ca-central-1":"Canada (Central)",
+      "cn-north-1": "China (Beijing)",
+      "cn-northwest-1": "China (Ningxia)",
+      "eu-central-1": "EU (Frankfurt)",
+      "eu-west-1": "EU (Ireland)",
+      "eu-west-2": "EU (London)",
+      "eu-west-3": "EU (Paris)",
+      "sa-east-1": "South America (Sao Paulo)"
+    }
 
 // Data.prototype.
 
