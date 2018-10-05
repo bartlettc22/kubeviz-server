b5a2e030430e597c4376220b022c4451e36055c2
Chris Bartlett <bartlettc@gmail.com>
2018-10-05 13:32:03 -0600

    Adding build steps

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VERSION=${1:-dev}
+GOOS=${2:-linux}
+DOCKER_REPO="bartlettc22/kubeviz-server"
+
+# Directory to house our binaries
+mkdir -p bin
+
+# Build the binary in Docker and extract it from the container
+docker build --build-arg VERSION=${VERSION} --build-arg GOOS=${GOOS} -t ${DOCKER_REPO}:${VERSION}-${GOOS} ./
+docker run --rm --name kubeviz-server-build -d --entrypoint "" ${DOCKER_REPO}:${VERSION}-${GOOS} sh -c "sleep 30"
+docker cp kubeviz-server-build:/usr/bin/kubeviz-server bin
+docker stop kubeviz-server-build
+
+# Zip up the binary
+cd bin
+zip kubeviz-server-${GOOS}-${VERSION}.zip kubeviz-server
+rm kubeviz-server
---
+++ scripts/publish_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SOURCE_VERSION=${1:-master}
+PUBLISH_VERSION=${2:-$SOURCE_VERSION}
+DOCKER_REPO="bartlettc22/kubeviz-server"
+
+docker tag ${DOCKER_REPO}:${SOURCE_VERSION}-linux ${DOCKER_REPO}:${PUBLISH_VERSION}
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${DOCKER_REPO}:${PUBLISH_VERSION}
