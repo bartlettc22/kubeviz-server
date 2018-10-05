03349124eeb29013212641cb44b87205cbb94934
Chris Bartlett <bartlettc@gmail.com>
2018-10-05 16:44:03 -0600

    Removing old node code

--- src/routes/stats.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var stats_controller = require('../controllers/statsController');
-
-router.get('/', stats_controller.sendStats)
-
-module.exports = router;
